a68771ba1da988384327e60c65f0e6ba1b14a063
mxw723 <mxw723@student.bham.ac.uk>
2018-11-03 16:17:34 +0000

    Connections for each player

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,18 +1,23 @@
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.net.*;
 
 public class Launcher {
+	
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
+		ServerSocket jSConnection;
+		Board board = new Board();
+		board.init();
 		try {
-			ServerSocket jSConnection = new ServerSocket(8080);
+			jSConnection = new ServerSocket(8080);
+			Socket pOneSocket = jSConnection.accept();
+			Socket pTwoSocket = jSConnection.accept();
+			
 		} catch (IOException e) {
 			System.out.println("Error opening socket");
 			e.printStackTrace();
+			return;
 		}
-		
-		Board board = new Board();
-		board.init();
-		board.print();
+		//board.print();
 	}
 }
---
+++ src/PlayerConnection.java
@@ -0,0 +1,10 @@
+
+public class PlayerConnection extends Thread {
+
+	Launcher master;
+	Tile team;
+	
+	public PlayerConnection(Launcher master, Tile player) {
+		this.master = master
+	}
+}
