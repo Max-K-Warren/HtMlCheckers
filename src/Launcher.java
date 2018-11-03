389bdf8673d39b94804c48d34544a000a0d0f3ff
mxw723 <mxw723@student.bham.ac.uk>
2018-11-03 15:55:11 +0000

    Setting up server

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,7 +1,16 @@
+import java.io.IOException;
+import java.net.ServerSocket;
 
 public class Launcher {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
+		try {
+			ServerSocket jSConnection = new ServerSocket(8080);
+		} catch (IOException e) {
+			System.out.println("Error opening socket");
+			e.printStackTrace();
+		}
+		
 		Board board = new Board();
 		board.init();
 		board.print();
