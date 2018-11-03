56d7603b1ee7ad5b7cc2cfc9ab4342f6ae5cd66c
mxw723 <mxw723@student.bham.ac.uk>
2018-11-03 22:34:00 +0000

    Working server to HTTP

---
+++ src/NameGiver.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+import javax.xml.ws.spi.http.HttpExchange;
+import javax.xml.ws.spi.http.HttpHandler;
+
+public class NameGiver extends HttpHandler {
+
+	@Override
+	public void handle(HttpExchange arg0) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
